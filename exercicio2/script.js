841065bf08a91b3b8189e5041dc83a99f826738a
Euvinni <marcosvdsc1212@gmail.com>
2023-02-28 21:17:32 -0300

    adicionado exercicios de condicionais

---
+++ exercicio2/script.js
@@ -0,0 +1,35 @@
+function estudanteUsuario() {
+const idade = 17
+const ensinoMedioCompleto = true
+const cursandoFaculdade = false
+if(idade >= 18){
+console.log(`A pessoa é maior de idade`)
+} else {
+console.log(`A pessoa é menor de idade`)
+} 
+if(ensinoMedioCompleto){
+console.log(`A pessoa tem ensino médio completo`)    
+} else {
+console.log(`A pessoa não tem ensino médio completo`)    
+}
+if(cursandoFaculdade) {
+console.log(`A pessoa está cursando faculdade`)    
+} else {
+console.log(`A pessoa não está cursando faculdade`)    
+}
+}
+
+estudanteUsuario()
+
+
+
+
+
+
+
+
+
+
+
+
+
